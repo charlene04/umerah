862fce92e1234e2e9fc87b8d4d86343e6cb50b32
charlene04 <charlesugbana04@gmail.com>
2021-05-19 22:26:26 +0100

    new

--- umerahscrumy/models.py
+++ umerahscrumy/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 # Create your models here.
 class GoalStatus(models.Model):
     status_name = models.CharField(max_length = 120)
+    def __str__(self):
+        return self.status_name
     
 
 class ScrumyGoals(models.Model):
@@ -14,7 +16,8 @@ class ScrumyGoals(models.Model):
     goal_status = models.ForeignKey(GoalStatus, on_delete = models.PROTECT)
     user = models.ForeignKey(User, related_name= 'maps', on_delete = models.CASCADE)
   
-
+    def __str__(self):
+        return self.goal_name
 
 
 class ScrumyHistory(models.Model):
@@ -24,6 +27,10 @@ class ScrumyHistory(models.Model):
     moved_to = models.CharField(max_length = 120)
     time_of_action = models.TimeField()
     goal = models.ForeignKey(ScrumyGoals, on_delete = models.CASCADE)
+    
+    def __str__(self):
+
+        return self.created_by
 
   
 
